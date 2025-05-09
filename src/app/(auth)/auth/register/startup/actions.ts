459d41573392b0cbe6ed1feaed84bb4467b5e3f8
DeadInsane5 <105852185+DeadInsane5@users.noreply.github.com>
2025-05-09 10:51:01 +0530

    production

--- src/app/(auth)/auth/register/startup/actions.ts
+++ src/app/(auth)/auth/register/startup/actions.ts
@@ -4,17 +4,19 @@ import { redirect } from "next/navigation"
 import { createClient } from "@/lib/supabase/server"
 import { revalidatePath } from "next/cache"
 
-export async function signUp(formData: FormData) {
+export async function signUpStartUp(formData: FormData) {
     const supabase = await createClient();
     const signUpData = {
         email: formData.get('email') as string,
         password: formData.get('password') as string,
     };
-    const insertData = {
-        id: 1,
-        first_name: formData.get('first_name') as string,
-        last_name: formData.get('last_name') as string,
-        email: formData.get('email') as string,
+    const profileData = {
+        full_name: formData.get('first_name') as string + ' ' + formData.get('last_name') as string,
+        user_type: 'startup',
+        avatar_url : "",
+        bio: formData.get('bio') as string,
+        website: formData.get('website') as string,
+        location: formData.get('location') as string,
     }
 
     const { error: signUpError } = await supabase.auth.signUp(signUpData)
@@ -23,12 +25,33 @@ export async function signUp(formData: FormData) {
         throw new Error(signUpError.message);
     }
 
-    const { error: insertError } = await supabase.from('users').insert(insertData);
+    const { error: profileInsertError } = await supabase.from('profiles').insert(profileData);
 
-    if (insertError) {
-        throw new Error(insertError.message);
+    if (profileInsertError) {
+        throw new Error(profileInsertError.message);
     }
+}
 
+export async function registerStartup(formData:FormData) {
+    const supabase = await createClient();
+
+    const registerStartupData = {
+        startup_name: formData.get('startup_name') as string,
+        logo_url: formData.get('logo_url') as string,
+        tagline: formData.get('tagline') as string,
+        description: formData.get('description') as string,
+        industry: formData.get('industry') as string,
+        development_stage: formData.get('development_stage') as string,
+        founded_date: formData.get('founded_date') as string,
+        team_size: formData.get('team_size') as string,
+        funding_goal: formData.get('funding_goal') as string,
+    };
+
+    const {error: registerStartupError} = await supabase.from('startups').insert(registerStartupData);
+
+    if (registerStartupError) {
+        throw new Error(registerStartupError.message);
+    }
     revalidatePath('/', 'layout');
     redirect('/');
 }
--- src/app/(auth)/auth/register/startup/page.tsx
+++ src/app/(auth)/auth/register/startup/page.tsx
@@ -1,15 +1,20 @@
 "use client"
-import { useRouter } from "next/navigation"
-import { ArrowRight, RocketIcon } from "lucide-react"
+import { ArrowDownIcon, ChevronDown, RocketIcon } from "lucide-react"
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { signUp } from "./actions"
+import { registerStartup, signUpStartUp } from "./actions"
+import { useState } from "react"
+import { Textarea } from "@/components/ui/textarea"
+import { Select, SelectContent, SelectItem, SelectValue } from "@/components/ui/select"
+import { Arrow, SelectTrigger } from "@radix-ui/react-select"
+import { Label } from "@/components/ui/label"
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu"
 
 export default function Register() {
-    const router = useRouter()
+    const [registered, setRegistered] = useState(false)
 
     return (
         <>
@@ -21,49 +26,133 @@ export default function Register() {
                             Register as a Startup
                         </CardTitle>
                         <CardDescription>Create your startup profile to connect with investors, customers, and resources.</CardDescription>
-                        <CardContent className="p-2">
-                            <form action={signUp}>
-                                <div className="personal">
-                                    <div className="row py-2 flex gap-4">
-                                        <div className="input flex flex-col gap-2">
-                                            <label htmlFor="first_name">First Name</label>
-                                            <Input type="first_name" id="first_name" name="first_name" className="first_name" required></Input>
-                                        </div>
-                                        <div className="input flex flex-col gap-2">
-                                            <label htmlFor="last_name">Last Name</label>
-                                            <Input type="last_name" id="last_name" name="last_name" className="last_name" required></Input>
-                                        </div>
+                        <CardContent className={!registered ? "block p-2" : "hidden"}>
+                            <form action={signUpStartUp}>
+                                <div className="row py-2 flex gap-4">
+                                    <div className="input flex flex-col gap-2">
+                                        <Label htmlFor="first_name">First Name*</Label>
+                                        <Input type="first_name" id="first_name" name="first_name" className="first_name" required></Input>
                                     </div>
-                                    <div>
-                                        <label htmlFor="email">Email</label>
-                                        <Input type="email" id="email" name="email" className="email my-2" required></Input>
-                                    </div>
-                                    <div>
-                                        <label htmlFor="password">Password</label>
-                                        <Input type="password" id="password" name="password" className="password my-2" required></Input>
-                                    </div>
-                                    <div>
-                                        <label htmlFor="confirm_password">Confirm Password</label>
-                                        <Input type="password" id="confirm_password" name="confirm_password" className="confirm_password my-2" required></Input>
+                                    <div className="input flex flex-col gap-2">
+                                        <Label htmlFor="last_name">Last Name*</Label>
+                                        <Input type="last_name" id="last_name" name="last_name" className="last_name" required></Input>
                                     </div>
                                 </div>
-                                <Button type="submit" className="my-2 w-full">Continue to Profile</Button>
+                                <div>
+                                    <Label htmlFor="email">Email*</Label>
+                                    <Input type="email" id="email" name="email" className="email my-2" required></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input type="password" id="password" name="password" className="password my-2" required></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="confirm_password">Confirm Password</Label>
+                                    <Input type="password" id="confirm_password" name="confirm_password" className="confirm_password my-2" required></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="avatar">Upload Avatar</Label>
+                                    <Input type="file" id="avatar" name="avatar" className="avatar w-full h-20 my-2"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="bio">Bio</Label>
+                                    <Textarea id="bio" name="bio" className="bio my-2 h-30"></Textarea>
+                                </div>
+                                <div>
+                                    <Label htmlFor="website">Website</Label>
+                                    <Input type="text" id="website" name="website" className="website my-2"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="location">Location</Label>
+                                    <Input type="text" id="location" name="location" className="location my-2"></Input>
+                                </div>
+                                <div className="register">
+                                    <Button type="submit" className="my-2 w-full" onClick={() => setRegistered(true)}>Complete Profile</Button>
+                                </div>
                             </form>
-                            <CardFooter>
-                                <div className="footer flex flex-col gap-2">
-                                    <p className="text-sm text-muted-foreground text-center">By registering, you agree to our Terms of Service and Privacy Policy.</p>
-                                    <div className="text-center text-sm">
-                                        Already have an account?{" "}
-                                        <a href="/auth/login" className="underline underline-offset-4">
-                                            Log in
-                                        </a>
+                        </CardContent>
+                        <CardContent className={registered ? "block p-2" : "hidden"}>
+                            <form action={registerStartup}>
+                                <div>
+                                    <Label htmlFor="startpup_name">Startup Name</Label>
+                                    <Input type="text" id="startup_name" name="startup_name" className="startup_name my-2" required></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="logo">Logo</Label>
+                                    <Input type="file" id="logo" name="logo" className="logo my-2 h-20"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="tagline">Tagline</Label>
+                                    <Input type="text" id="tagline" name="tagline" className="tagline my-2"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="description">Description</Label>
+                                    <Textarea id="description" name="description" className="description my-2 h-30"></Textarea>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4">
+                                    <div className="space-y-2">
+                                        <Label htmlFor="industry">Industry</Label>
+                                        <Select>
+                                            <SelectTrigger id="industry" className="px-4 h-10 bg-muted rounded-md">
+                                            <SelectValue placeholder={<div className="flex gap-2"><Label>Select Industry</Label><ChevronDown className="size-4" /></div>} />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectItem value="technology">Technology</SelectItem>
+                                                <SelectItem value="healthcare">Healthcare</SelectItem>
+                                                <SelectItem value="finance">Finance</SelectItem>
+                                                <SelectItem value="education">Education</SelectItem>
+                                                <SelectItem value="ecommerce">E-commerce</SelectItem>
+                                                <SelectItem value="other">Other</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                    <div className="space-y-2">
+                                        <Label htmlFor="stage">Development Stage</Label>
+                                        <Select>
+                                            <SelectTrigger id="stage" className="px-4 h-10 bg-muted rounded-md">
+                                            <SelectValue placeholder={<div className="flex gap-2"><Label>Select Stage</Label><ChevronDown className="size-4" /></div>} />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectItem value="idea">Idea Stage</SelectItem>
+                                                <SelectItem value="mvp">MVP</SelectItem>
+                                                <SelectItem value="early">Early Traction</SelectItem>
+                                                <SelectItem value="growth">Growth</SelectItem>
+                                                <SelectItem value="scale">Scaling</SelectItem>
+                                            </SelectContent>
+                                        </Select>
                                     </div>
                                 </div>
-                            </CardFooter>
+                                <div>
+                                    <Label htmlFor="founded_date">Founded_date</Label>
+                                    <Input id="text" name="founded_date" className="founded_date my-2"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="team_size">Team Size</Label>
+                                    <Input type="text" id="team_size" name="team_size" className="team_size my-2"></Input>
+                                </div>
+                                <div>
+                                    <Label htmlFor="funding_goal">Funding Goal</Label>
+                                    <Input type="text" id="funding_goal" name="funding_goal" className="funding_goal my-2"></Input>
+                                </div>
+                                <div className="register">
+                                    <Button type="submit" disabled={!registered} className="my-2 w-full">Complete Registration</Button>
+                                </div>
+                            </form>
                         </CardContent>
+                        <CardFooter className={!registered ? "block" : "hidden"}>
+                            <div className="footer flex flex-col gap-2">
+                                <p className="text-sm text-muted-foreground text-center">By registering, you agree to our Terms of Service and Privacy Policy.</p>
+                                <div className="text-center text-sm">
+                                    Already have an account?{" "}
+                                    <a href="/auth/login" className="underline underline-offset-4">
+                                        Log in
+                                    </a>
+                                </div>
+                            </div>
+                        </CardFooter>
                     </CardHeader>
                 </Card>
-            </div>
+            </div >
         </>
     )
 }
